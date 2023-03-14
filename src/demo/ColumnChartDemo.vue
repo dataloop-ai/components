b5e91b1aee7fbe5b3e3480b46812090a0f687936
Fadi Atamny <fadi@dataloop.ai>
2023-03-14 15:35:49 +0200

    fixed all lodash imports

--- src/demo/ColumnChartDemo.vue
+++ src/demo/ColumnChartDemo.vue
@@ -59,7 +59,7 @@ import { defineComponent } from 'vue-demi'
 import DlColumnChart from '../components/DlChart/DlColumnChart.vue'
 import DlAvatar from '../components/DlAvatar.vue'
 
-import orderBy from 'lodash/orderBy'
+import { orderBy } from 'lodash'
 
 import {
     Chart as ChartJS,
