823eca944fd14ead8a5bcaf8aab8d21d92e6f606
Vadim Popa <vadim.popa@opentech.dev>
2023-04-04 13:02:19 +0300

    fix vue2

--- src/utils/dynamicSvgImport.ts
+++ src/utils/dynamicSvgImport.ts
@@ -1,5 +1,10 @@
+import { isVue3 } from 'vue-demi'
+
 export function dynamicSvgImport(iconName: string) {
-    return import(
-        `../../node_modules/@dataloop-ai/icons/assets/${iconName}.svg`
-    ).then((d) => d.default)
+    const prefixVue3 = `../../`
+    const prefixVue2 = `../../../../../`
+    const path = `${
+        isVue3 ? prefixVue3 : prefixVue2
+    }node_modules/@dataloop-ai/icons/assets/${iconName}.svg`
+    return import(path).then((d) => d.default)
 }
