8fb5bd27e67e4034d5e43edf0c30d1004872f992
Sorin <127127362+SorinOpt@users.noreply.github.com>
2023-04-13 16:22:05 +0300

    fix chart zooming on updating brush range (#143)
    Co-authored-by: fadiDL <99925584+fadiDL@users.noreply.github.com>

--- src/components/compound/DlCharts/charts/DlColumnChart/DlColumnChart.vue
+++ src/components/compound/DlCharts/charts/DlColumnChart/DlColumnChart.vue
@@ -101,6 +101,7 @@ import {
 import type { Chart, ChartMeta, ChartDataset, ActiveElement } from 'chart.js'
 import { unionBy, orderBy, merge, isEqual } from 'lodash'
 import { useThemeVariables } from '../../../../../hooks/use-theme'
+import { getMaxDatasetValue } from '../../utils'
 
 ChartJS.register(
     Title,
@@ -365,7 +366,17 @@ export default defineComponent({
         const chartOptions = reactive(
             updateKey(
                 merge(
-                    { onResize, onHover },
+                    {
+                        onResize,
+                        onHover,
+                        scales: {
+                            y: {
+                                suggestedMax: getMaxDatasetValue(
+                                    chartData.value.datasets
+                                )
+                            }
+                        }
+                    },
                     defaultColumnChartProps.options,
                     props.options
                 ),
