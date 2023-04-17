9cda36983b1e8c6e0b5d9a90032d5093ab2d4d90
Fadi Atamny <fadi@dataloop.ai>
2023-04-17 17:08:48 +0300

    added favicon to storybook

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,3 +4,9 @@ import { mainTheme } from './themes'
 addons.setConfig({
     theme: mainTheme
 })
+
+// Import favicon
+const link = document.createElement('link')
+link.setAttribute('rel', 'shortcut icon')
+link.setAttribute('href', '../public/favicon.ico')
+document.head.appendChild(link)
