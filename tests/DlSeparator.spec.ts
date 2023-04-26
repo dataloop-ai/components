a06b967f3c8a24c29219f494ea9c5c642613c15d
Denis Fedorovici <denis.fedorovici@opentech.dev>
2023-04-26 11:09:10 +0300

    DLCL-901

--- tests/DlSeparator.spec.ts
+++ tests/DlSeparator.spec.ts
@@ -1,22 +1,33 @@
 import { mount } from '@vue/test-utils'
 import { DlSeparator } from '../src/components'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeAll } from 'vitest'
 
 describe('DlSeparator', () => {
-    it('should render the separator', () => {
-        const wrapper = mount(DlSeparator, {
-            props: {
-                color: 'red',
-                height: '400px',
-                type: 'vertical'
-            }
-        })
+    describe('When mounting', () => {
+        let wrapper: any
 
-        expect(wrapper.exists()).toBe(true)
-        expect((wrapper.element as HTMLElement).style.height).toEqual('400px')
-        expect((wrapper.element as HTMLElement).style.backgroundColor).toEqual(
-            'red'
-        )
-        expect(wrapper.vm.type).toBe('vertical')
+        beforeAll(() => {
+            wrapper = mount(DlSeparator, {
+                props: {
+                    color: 'red',
+                    height: '400px',
+                    type: 'vertical'
+                }
+            })
+        })
+        it('should exist component', function () {
+            expect(wrapper.exists()).toBe(true)
+        })
+        it('should have the right styles', function () {
+            expect((wrapper.element as HTMLElement).style.height).toEqual(
+                '400px'
+            )
+            expect(
+                (wrapper.element as HTMLElement).style.backgroundColor
+            ).toEqual('red')
+        })
+        it('should have the right props', function () {
+            expect(wrapper.vm.type).toBe('vertical')
+        })
     })
 })
--- tests/DlSkeleton.spec.ts
+++ tests/DlSkeleton.spec.ts
@@ -1,19 +1,26 @@
 import { mount } from '@vue/test-utils'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeAll } from 'vitest'
 import { DlSkeleton } from '../src'
 
 describe('DlSkeleton', () => {
-    it('should have correct dimensions given', () => {
-        const wrapper = mount(DlSkeleton, {
-            props: {
-                width: '200px',
-                height: '200px'
-            }
+    describe('When mounting', () => {
+        let wrapper: any
+        let element: any
+        beforeAll(() => {
+            wrapper = mount(DlSkeleton, {
+                props: {
+                    width: '200px',
+                    height: '200px'
+                }
+            })
+            element = wrapper.find('.skeleton-box')?.element as HTMLElement
         })
-
-        const element = wrapper.find('.skeleton-box')?.element as HTMLElement
-        expect(element).not.toBe(undefined)
-        expect(element.style.width).toEqual('200px')
-        expect(element.style.height).toEqual('200px')
+        it('should exist component', function () {
+            expect(element).not.toBe(undefined)
+        })
+        it('should have the right styles', function () {
+            expect(element.style.width).toEqual('200px')
+            expect(element.style.height).toEqual('200px')
+        })
     })
 })
