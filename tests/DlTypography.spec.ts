c0cfc671343ab8a8cb68ab657e22a2ddd99b93f6
Denis Fedorovici <denis.fedorovici@indrivo.com>
2023-04-28 18:24:00 +0300

    DLCL-901

--- tests/DlTypography.spec.ts
+++ tests/DlTypography.spec.ts
@@ -1,28 +1,37 @@
 import { mount } from '@vue/test-utils'
-import { describe, it, expect } from 'vitest'
+import { beforeAll, describe, expect, it } from 'vitest'
 import { DlTypography } from '../src/components'
 
 describe('DlTypography', () => {
-    it('should compute values properly', async () => {
-        const wrapper = mount(DlTypography, {
-            props: {
-                uppercase: true,
-                bold: false,
-                size: '30px'
-            },
-            slots: {
-                default: () => 'some'
-            }
+    describe('When mounting', () => {
+        let wrapper: any
+
+        beforeAll(() => {
+            wrapper = mount(DlTypography, {
+                props: {
+                    uppercase: true,
+                    bold: false,
+                    size: '30px'
+                },
+                slots: {
+                    default: () => 'some'
+                }
+            })
         })
-
-        expect(wrapper.html()).toBe(
-            `<p data-v-50b0b5ef="" id="${wrapper.vm.uuid}" class="dl-typography" style="text-transform: uppercase; font-weight: 400; font-size: 30px;">some</p>`
-        )
-
-        await wrapper.setProps({ uppercase: false, bold: true })
-
-        expect(wrapper.html()).toBe(
-            `<p data-v-50b0b5ef="" id="${wrapper.vm.uuid}" class="dl-typography" style="text-transform: none; font-weight: bold; font-size: 30px;">some</p>`
-        )
+        it('should right dom node html', function () {
+            expect(wrapper.html()).toBe(
+                `<p data-v-50b0b5ef="" id="${wrapper.vm.uuid}" class="dl-typography" style="text-transform: uppercase; font-weight: 400; font-size: 30px;">some</p>`
+            )
+        })
+        describe('When change props', () => {
+            beforeAll(async () => {
+                await wrapper.setProps({ uppercase: false, bold: true })
+            })
+            it('should right dom node styles', function () {
+                expect(wrapper.html()).toBe(
+                    `<p data-v-50b0b5ef="" id="${wrapper.vm.uuid}" class="dl-typography" style="text-transform: none; font-weight: bold; font-size: 30px;">some</p>`
+                )
+            })
+        })
     })
 })
