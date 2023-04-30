882b16d68629c3594567082cd740b647a4220d8b
Fadi Atamny <fadi@dataloop.ai>
2023-04-30 17:29:28 +0300

    fixed chip behavior

--- src/components/basic/DlChip/DlChip.vue
+++ src/components/basic/DlChip/DlChip.vue
@@ -217,13 +217,6 @@ export default defineComponent({
     background-color: var(--dl-chip-bg-color);
     border: var(--dl-chip-border);
 
-    .first-letter-capitalized {
-        &::first-letter,
-        & > *::first-letter {
-            text-transform: capitalize;
-        }
-    }
-
     &--content {
         cursor: default;
         width: auto;
@@ -237,6 +230,14 @@ export default defineComponent({
         overflow: hidden;
     }
 }
+
+.dl-chip.first-letter-capitalized {
+    &::first-letter,
+    & > *::first-letter {
+        text-transform: capitalize;
+    }
+}
+
 .dl-chip-remove-icon-container {
     cursor: var(--dl-chip-left-icon-cursor);
     position: absolute;
