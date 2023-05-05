d91c7e40583541f1ac03f4ad96f50427068adbaf
Prodan Eric <ericprodan1920s@gmail.com>
2023-05-05 16:47:32 +0300

    added background when dragging widgets

--- src/components/basic/DlWidget/DlWidget.vue
+++ src/components/basic/DlWidget/DlWidget.vue
@@ -217,7 +217,17 @@ export default defineComponent({
     }
 
     &__drag {
-        visibility: hidden;
+        position: relative;
+        &::after {
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            background: var(--dl-color-separator);
+            border-radius: 5px;
+        }
     }
 }
 
