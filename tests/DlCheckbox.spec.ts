d4dfff869647be9cea69611b30b3b8d5195bd592
Denis Fedorovici <denis.fedorovici@indrivo.com>
2023-05-12 17:16:14 +0300

    DLCL-901

--- tests/DlCheckbox.spec.ts
+++ tests/DlCheckbox.spec.ts
@@ -1,87 +1,95 @@
 import { mount } from '@vue/test-utils'
 import { DlCheckbox } from '../src/components'
-import { describe, it, expect } from 'vitest'
+import { beforeAll, describe, expect, it } from 'vitest'
 
 describe('DlCheckbox', () => {
-    it('should add value to v-model array', async () => {
-        const wrapper = mount(DlCheckbox, {
-            props: {
-                modelValue: [3, 4, 5],
-                value: 6
-            }
-        })
+    describe('When mounting', () => {
+        let wrapper: any
 
-        const label = wrapper.find('label')
-        await label.trigger('click')
-        expect(
-            (wrapper.emitted()['update:model-value'][0] as number[])[0]
-        ).toEqual([3, 4, 5, 6])
+        beforeAll(() => {
+            wrapper = mount(DlCheckbox, {
+                props: {
+                    modelValue: [3, 4, 5],
+                    value: 6
+                }
+            })
+        })
+        it('should mount the component', async () => {
+            expect(wrapper.exists()).toBe(true)
+        })
+        describe('When add value to v-model array', () => {
+            beforeAll(async () => {
+                const label = wrapper.find('label')
+                await label.trigger('click')
+            })
+            it('should have the right modelValue', function () {
+                expect(
+                    (wrapper.emitted()['update:model-value'][0] as number[])[0]
+                ).toEqual([3, 4, 5, 6])
+            })
+        })
+        describe('When emit event on Enter keypress', () => {
+            beforeAll(async () => {
+                const label = wrapper.find('label')
+                await label.trigger('keyup', { key: 'Enter' })
+            })
+            it('should have the right modelValue', function () {
+                expect(wrapper.emitted()['update:model-value']).toBeTruthy()
+            })
+        })
     })
+    describe('When emit value passed as false value', () => {
+        let wrapper: any
 
-    it('should remove value from v-model array', async () => {
-        const wrapper = mount(DlCheckbox, {
-            props: {
-                modelValue: [3, 4, 5, 6],
-                value: 6
-            }
+        beforeAll(async () => {
+            wrapper = mount(DlCheckbox, {
+                props: {
+                    modelValue: 'yes',
+                    trueValue: 'yes',
+                    falseValue: 'no'
+                }
+            })
+            const label = wrapper.find('label')
+            await label.trigger('click')
         })
-
-        const label = wrapper.find('label')
-        await label.trigger('click')
-        expect(
-            (wrapper.emitted()['update:model-value'][0] as number[])[0]
-        ).toEqual([3, 4, 5])
+        it('should have the right modelValue', function () {
+            expect(
+                (wrapper.emitted()['update:model-value'][0] as string)[0]
+            ).toEqual('no')
+        })
     })
+    describe('When set the proper color for disabled checkbox', () => {
+        let wrapper: any
 
-    it('should emit event on Enter keypress', async () => {
-        const wrapper = mount(DlCheckbox, {
-            props: {
-                modelValue: false
-            }
+        beforeAll(async () => {
+            wrapper = mount(DlCheckbox, {
+                props: {
+                    modelValue: false,
+                    disabled: true
+                }
+            })
         })
-
-        const label = wrapper.find('label')
-        await label.trigger('keyup', { key: 'Enter' })
-        expect(wrapper.emitted()['update:model-value']).toBeTruthy()
+        it('should have the right active color', function () {
+            const cssVars = wrapper.vm.cssSvgVars
+            expect(cssVars['--dl-active-color']).toBe(
+                'var(--dl-color-disabled)'
+            )
+        })
     })
+    describe('When compute isIndeterminate value properly', () => {
+        let wrapper: any
 
-    it('should emit value passed as false value', async () => {
-        const wrapper = mount(DlCheckbox, {
-            props: {
-                modelValue: 'yes',
-                trueValue: 'yes',
-                falseValue: 'no'
-            }
+        beforeAll(async () => {
+            wrapper = mount(DlCheckbox, {
+                props: {
+                    modelValue: 'sample value',
+                    indeterminateValue: 'sample value'
+                }
+            })
         })
-
-        const label = wrapper.find('label')
-        await label.trigger('click')
-        expect(
-            (wrapper.emitted()['update:model-value'][0] as string)[0]
-        ).toEqual('no')
-    })
-
-    it('should set the proper color for disabled checkbox', () => {
-        const wrapper = mount(DlCheckbox, {
-            props: {
-                modelValue: false,
-                disabled: true
-            }
+        it('should have the right isIndeterminate flag', function () {
+            expect(wrapper.vm.isIndeterminate).toBe(true)
         })
-
-        const cssVars = wrapper.vm.cssSvgVars
-        expect(cssVars['--dl-active-color']).toBe('var(--dl-color-disabled)')
-    })
-
-    it('should compute isIndeterminate value properly', () => {
-        const wrapper = mount(DlCheckbox, {
-            props: {
-                modelValue: 'sample value',
-                indeterminateValue: 'sample value'
-            }
-        })
-
-        expect(wrapper.vm.isIndeterminate).toBe(true)
     })
 
     describe('When having a sublabel', () => {
