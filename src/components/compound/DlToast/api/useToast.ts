c24e5fa18ef860ff8faf2882d947314326740254
fadiDL <99925584+fadiDL@users.noreply.github.com>
2023-05-15 15:18:51 +0300

    258 feature dltoast allow collapsable count (#260)
    * updated global classes
    
    * fixed alert and toast demo issues
    
    * added badge count ability
    
    * stored state for useToast to handle removing and reseting states and counts of similar components
    
    * vue2 support

--- src/components/compound/DlToast/api/useToast.ts
+++ src/components/compound/DlToast/api/useToast.ts
@@ -1,5 +1,12 @@
+import { isVue3 } from 'vue-demi'
 import toastComponent from '../components/ToastComponent.vue'
 import { createComponent } from '../utils/render'
+import { v4 } from 'uuid'
+
+const state: { prevToastId: any; toasts: { [key: string]: any } } = {
+    prevToastId: null,
+    toasts: {}
+}
 
 export const useToast = (globalProps = {}) => {
     return {
@@ -8,16 +15,46 @@ export const useToast = (globalProps = {}) => {
             if (typeof options === 'string') message = options
 
             const defaultProps = {
-                message
+                message,
+                identifier: v4()
             }
 
             const propsData = Object.assign(
                 {},
                 defaultProps,
                 globalProps,
-                options
+                options,
+                {
+                    remove: () => {
+                        delete state.toasts[defaultProps.identifier]
+                    }
+                }
             )
-            createComponent(toastComponent, propsData, document.body)
+
+            if (state.prevToastId && state.toasts[state.prevToastId]) {
+                const toast = state.toasts[state.prevToastId]
+                const props = isVue3 ? toast.props : toast.$children[0]
+                const similar =
+                    propsData.message === props.message &&
+                    propsData.type === props.type
+                if (similar) {
+                    if (isVue3) {
+                        toast.proxy.updateCount(toast.proxy.count + 1)
+                    } else {
+                        toast.$children[0].updateCount(
+                            toast.$children[0].count + 1
+                        )
+                    }
+                    return
+                }
+            }
+
+            state.prevToastId = defaultProps.identifier
+            state.toasts[defaultProps.identifier] = createComponent(
+                toastComponent,
+                propsData,
+                document.body
+            )
         }
     }
 }
