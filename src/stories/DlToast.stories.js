1cc2e72cc8d1e393117e6629675e2aed75c46ae0
fadiDL <99925584+fadiDL@users.noreply.github.com>
2023-05-21 20:20:35 +0300

    286 feature dltoast add predefined setups with sucess info error and warn (#287)
    * added new typing
    
    * updated imports and introduced new setups
    
    * updated demo and stories
    
    * fixed tests

--- src/stories/DlToast.stories.js
+++ src/stories/DlToast.stories.js
@@ -34,11 +34,6 @@ export default {
             description: 'number of seconds the component is displayed',
             defaultValue: 10
         },
-        classItem: {
-            control: 'text',
-            description: 'custom class for toast item',
-            defaultValue: 'demo-toast'
-        },
         width: {
             control: 'text',
             description: 'toast width size',
@@ -65,15 +60,8 @@ const Template = (args) => ({
     template: `<div style="padding: 50px"><DlButton @click="click" v-bind="args" /></div>`,
     methods: {
         click() {
-            const {
-                type,
-                message,
-                position,
-                duration,
-                closable,
-                classItem,
-                width
-            } = this.args
+            const { type, message, position, duration, closable, width } =
+                this.args
 
             DlToast.open({
                 type,
@@ -81,7 +69,6 @@ const Template = (args) => ({
                 position,
                 duration,
                 closable,
-                classItem,
                 width
             })
         }
