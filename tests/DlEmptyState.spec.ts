bd1a5b0d44709cd0d123e4f0b34acf4ffbb06fa2
Sorin <sorin.hadjiu@opentech.dev>
2023-06-01 11:20:59 +0300

    fix tests

--- tests/DlEmptyState.spec.ts
+++ tests/DlEmptyState.spec.ts
@@ -1,72 +1,41 @@
 import { mount } from '@vue/test-utils'
 import { DlEmptyState } from '../src/'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeAll } from 'vitest'
 
 describe('DlEmptyState', () => {
-    it('testing DlEmptyState functionality', async () => {
-        const wrapper = mount(DlEmptyState, {
-            props: {}
-        })
+    describe('When mounting', () => {
+        let wrapper: any
 
-        expect(wrapper.exists()).toBe(true)
-        expect(wrapper.props()).toStrictEqual({
-            align: 'center',
-            bgImage: null,
-            bgSize: '240px',
-            icon: 'icon-dl-alert-filled',
-            iconColor: 'dl-color-darker',
-            iconSize: '40px',
-            info: '',
-            infoColor: 'dl-color-darker',
-            infoSize: '14px',
-            responsive: false,
-            subtitle: '',
-            subtitleColor: 'dl-color-medium',
-            subtitleSize: '14px',
-            title: 'Something Went Wrong',
-            titleColor: 'dl-color-darker',
-            titleSize: '20px'
+        beforeAll(() => {
+            wrapper = mount(DlEmptyState, {
+                props: {}
+            })
         })
-
-        expect(wrapper.vm.iconClassName).toBe('empty-state--icon')
-        expect(wrapper.vm.titleClassName).toBe('empty-state--title')
-        expect(wrapper.vm.subtitleClassName).toBe('empty-state--subtitle')
-        expect(wrapper.vm.infoClassName).toBe('empty-state--info')
-        expect(wrapper.vm.cssVars).toEqual({
-            '--bg-image': null,
-            '--bg-size': '240px'
+        it('should mount the component', function () {
+            expect(wrapper.exists()).toBe(true)
         })
-
-        await wrapper.setProps({
-            bgImage: 'url(random-image.org)'
+        it('should compute the correct classes when calling computeClass', function () {
+            expect(wrapper.vm.iconClassName).toBe('empty-state--icon')
+            expect(wrapper.vm.titleClassName).toBe('empty-state--title')
+            expect(wrapper.vm.subtitleClassName).toBe('empty-state--subtitle')
+            expect(wrapper.vm.infoClassName).toBe('empty-state--info')
         })
+    })
 
-        expect(wrapper.props()).toStrictEqual({
-            bgImage: 'url(random-image.org)',
-            align: 'center',
-            bgSize: '240px',
-            icon: 'icon-dl-alert-filled',
-            iconColor: 'dl-color-darker',
-            iconSize: '40px',
-            info: '',
-            infoColor: 'dl-color-darker',
-            infoSize: '14px',
-            responsive: false,
-            subtitle: '',
-            subtitleColor: 'dl-color-medium',
-            subtitleSize: '14px',
-            title: 'Something Went Wrong',
-            titleColor: 'dl-color-darker',
-            titleSize: '20px'
+    describe('When set bgImage size prop', () => {
+        let wrapper: any
+        beforeAll(() => {
+            wrapper = mount(DlEmptyState, {
+                props: {
+                    bgSize: '400px'
+                }
+            })
         })
-
-        expect(wrapper.vm.iconClassName).toBe('empty-state--icon')
-        expect(wrapper.vm.titleClassName).toBe('empty-state--title')
-        expect(wrapper.vm.subtitleClassName).toBe('empty-state--subtitle')
-        expect(wrapper.vm.infoClassName).toBe('empty-state--info')
-        expect(wrapper.vm.cssVars).toEqual({
-            '--bg-image': 'url(random-image.org)',
-            '--bg-size': '240px'
+        it('should have a warn', function () {
+            expect(wrapper.vm.cssVars).toEqual({
+                '--bg-image': null,
+                '--bg-size': '400px'
+            })
         })
     })
 })
