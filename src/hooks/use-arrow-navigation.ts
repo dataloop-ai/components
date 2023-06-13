5c0c7e4f0bee93cc742c95b25bbb0895a4aa9fb6
Vadim Popa <vadim.popa@opentech.dev>
2023-06-13 15:43:45 +0300

    refactoring useArrowNavigation

--- src/hooks/use-arrow-navigation.ts
+++ src/hooks/use-arrow-navigation.ts
@@ -1,41 +1,84 @@
-import { ref, onMounted, onBeforeUnmount } from 'vue-demi'
+import { ref, isRef, onMounted, onBeforeUnmount, computed } from 'vue-demi'
+
+type ItemType = Record<string, any> | string
 
 export function useArrowNavigation(items: any, isOpen: any) {
-    const selectedItem = ref(null)
-    const highlightedIndex = ref(-1)
+    const selectItem = ref<ItemType | null>(null)
+    const highlightIndex = ref<number>(-1)
 
-    const handleArrowUp = () => {
-        if (highlightedIndex.value > 0) {
-            highlightedIndex.value--
+    const itemsData = computed(() => (isRef(items) ? items.value : items))
+    const isMenuOpen = computed(() => (isRef(isOpen) ? isOpen.value : isOpen))
+
+    const selectedItem = computed({
+        get: () => selectItem.value,
+        set: (value: ItemType) => {
+            selectItem.value = value
+        }
+    })
+    const highlightedIndex = computed({
+        get: () => highlightIndex.value,
+        set: (value: number) => {
+            highlightIndex.value = value
+        }
+    })
+
+    const itemsLength = computed<number>(() => itemsData.value.length)
+
+    const decrementHighlightedIndex = (): void => {
+        highlightedIndex.value -= 1
+    }
+    const incrementHighlightedIndex = (): void => {
+        highlightedIndex.value += 1
+    }
+    const resetHighlightedIndex = (): void => {
+        highlightedIndex.value = -1
+    }
+
+    const isEligibleToHandleEnter = computed<boolean>(() => {
+        return (
+            highlightedIndex.value >= 0 &&
+            highlightedIndex.value < itemsLength.value
+        )
+    })
+    const isEligibleToIncrementHighlightedIndex = computed<boolean>(() => {
+        return highlightedIndex.value < itemsLength.value - 1
+    })
+    const isEligibleToDecrementHighlightedIndex = computed<boolean>(() => {
+        return highlightedIndex.value > 0
+    })
+    const isNotEligibleToHandleArrows = computed<boolean>(() => {
+        return !itemsLength.value || !isMenuOpen.value
+    })
+
+    const handleArrowUp = (event: KeyboardEvent) => {
+        event.preventDefault()
+        if (isEligibleToDecrementHighlightedIndex) {
+            decrementHighlightedIndex()
         }
     }
-    const handleArrowDown = () => {
-        if (highlightedIndex.value < items.value.length - 1) {
-            highlightedIndex.value++
+    const handleArrowDown = (event: KeyboardEvent) => {
+        event.preventDefault()
+        if (isEligibleToIncrementHighlightedIndex.value) {
+            incrementHighlightedIndex()
         }
     }
     const handleEnter = () => {
-        if (
-            highlightedIndex.value >= 0 &&
-            highlightedIndex.value < items.value.length
-        ) {
-            selectedItem.value = items.value[highlightedIndex.value]
-            resetNavigation()
+        if (isEligibleToHandleEnter.value) {
+            selectedItem.value = itemsData.value[highlightedIndex.value]
+            resetHighlightedIndex()
         }
     }
-    const resetNavigation = () => {
-        highlightedIndex.value = -1
-    }
+
     const navigateList = (event: KeyboardEvent) => {
-        if (!items.value?.length || !isOpen.value) {
+        if (isNotEligibleToHandleArrows.value) {
             return
         }
         switch (event.key) {
             case 'ArrowUp':
-                handleArrowUp()
+                handleArrowUp(event)
                 break
             case 'ArrowDown':
-                handleArrowDown()
+                handleArrowDown(event)
                 break
             case 'Enter':
                 handleEnter()
