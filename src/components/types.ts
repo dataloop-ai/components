9b0b37b23dc34652b123bb6190c645da58f9e18c
Fadi Atamny <fadi@dataloop.ai>
2023-07-16 18:56:59 +0300

    exported shared types

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,5 +1,7 @@
+export * from './shared/types'
 export * from './essential/types'
 export * from './basic/types'
 export * from './compound/types'
+
 import { TInputSizes as DlInputSizes } from '../utils/input-sizes'
 export type { DlInputSizes }
