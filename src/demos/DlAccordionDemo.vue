19f612248e73c7796e53e034fd0c22c297a548d0
Vadim <127128115+vadim-popa-opentech@users.noreply.github.com>
2023-07-31 19:31:42 +0300

    the render slots have fixed (#518)

--- src/demos/DlAccordionDemo.vue
+++ src/demos/DlAccordionDemo.vue
@@ -13,6 +13,42 @@
             </dl-accordion>
         </div>
         <div style="width: 100%">
+            <p>Accordion in an accordion</p>
+            <dl-accordion
+                default-opened
+                title="Title Accordion in an accordion"
+            >
+                <template #header>
+                    Custom header content 1
+                    <dl-switch
+                        v-model="switchModel"
+                        :value="2"
+                    />
+                </template>
+
+                <dl-accordion default-opened>
+                    <template #header>
+                        Custom header content 2
+                        <dl-switch
+                            v-model="switchModel"
+                            :value="2"
+                        />
+                    </template>
+                    <template #default>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </template>
+                </dl-accordion>
+            </dl-accordion>
+        </div>
+        <div style="width: 100%">
             <dl-accordion
                 additional-info="Some additional info"
                 title="Uncontrolled with Lorem Ipsum"
