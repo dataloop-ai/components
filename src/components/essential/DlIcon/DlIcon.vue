6660e9df180efcb4710db05b6cb8216c5cceef31
Fadi Atamny <fadi@dataloop.ai>
2023-08-31 18:43:49 +0300

    hotfix2 colors

--- src/components/essential/DlIcon/DlIcon.vue
+++ src/components/essential/DlIcon/DlIcon.vue
@@ -157,7 +157,7 @@ export default defineComponent({
 <style scoped lang="scss">
 .dl-icon {
     display: inline-flex;
-    color: var(var(--dl-icon-color), var(--icon-color));
+    color: var(--dl-icon-color, var(--icon-color));
     font-size: var(--dl-icon-font-size);
 }
 </style>
