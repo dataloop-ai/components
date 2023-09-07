ba664779e5e94e81aa22d405c9b42e81028ea164
Adva Alkalay <adva.a@dataloop.ai>
2023-09-07 16:20:30 +0300

    prevent nested $and?

--- src/utils/parse-smart-query.ts
+++ src/utils/parse-smart-query.ts
@@ -211,13 +211,13 @@ export const stringifySmartQuery = (query: { [key: string]: any }): string => {
 
         if (key === '$and') {
             if (Array.isArray(value)) {
-                const andObject: { [key: string]: any } = {}
-                for (const subQuery of value) {
-                    for (const subKey in subQuery) {
-                        andObject[subKey] = subQuery[subKey]
-                    }
-                }
-                return stringifySmartQuery(andObject)
+                // should prevent the nested $and from being processed
+                const subQueries = value.map(
+                    (subQuery: { [key: string]: any }) =>
+                        stringifySmartQuery(subQuery)
+                )
+                result += subQueries.join(' AND ')
+                continue // Important: skip the subsequent processing for this $and key
             }
         }
 
