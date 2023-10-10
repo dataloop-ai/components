56423dfb0b0bd0a688a7270dcf0022de98ecc055
jigneshdavara314 <88855637+jigneshdavara314@users.noreply.github.com>
2023-10-10 19:42:57 +0530

    Update splitByQuotes.ts
    #585 Fix for search

--- src/utils/splitByQuotes.ts
+++ src/utils/splitByQuotes.ts
@@ -1,32 +1,44 @@
-export function splitByQuotes(input: string, split: string) {
-    const pattern = '([\\s\\S]*?)(e)?(?:(o)|(c)|(t)|(sp)|$)'
-        .replace('sp', split)
-        .replace('o', '[\\(\\{\\[]')
-        .replace('c', '[\\)\\}\\]]')
-        .replace('t', '[\'"]')
-        .replace('e', '[\\\\]')
-    const r = new RegExp(pattern, 'gi')
-    const stack: string[] = []
-    let buffer: string[] = []
-    const results: string[] = []
-    input.replace(r, ($0, $1, $e, $o, $c, $t, $s, i): any => {
-        if ($e) {
-            buffer.push($1, $s || $o || $c || $t)
-            return
-        } else if ($o) stack.push($o)
-        else if ($c) stack.pop()
-        else if ($t) {
-            if (stack[stack.length - 1] !== $t) stack.push($t)
-            else stack.pop()
-        } else {
-            if ($s ? !stack.length : !$1) {
-                buffer.push($1)
-                results.push(buffer.join(''))
-                buffer = []
-                return
+export function splitByQuotes(input: string, split: string): string[] {
+    const stack: string[] = [];
+    let buffer: string = '';
+    let escapeNext = false;
+    const results: string[] = [];
+
+    for (let char of input) {
+        // Handle escaped characters
+        if (escapeNext) {
+            buffer += char;
+            escapeNext = false;
+            continue;
+        }
+        if (char === '\\') {
+            buffer += char;
+            escapeNext = true;
+            continue;
+        }
+        // Handle quotes (both double and single)
+        if (char === '"' || char === "'") {
+            if (stack.length === 0) {
+                stack.push(char);
+            } else if (stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
             }
         }
-        buffer.push($0)
-    })
-    return results
+
+        // Handle the split character
+        if (char === split && stack.length === 0) {
+            results.push(buffer.trim());
+            buffer = '';
+        } else {
+            buffer += char;
+        }
+    }
+
+    if (buffer.trim()) {
+        results.push(buffer.trim());
+    }
+
+    return results;
 }
