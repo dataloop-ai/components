dataloop-ai/components

commit c07e0da8438c60189343a59290fa3351a2d1ff8e

DAT-54078 (#634)
* DAT-54078

* a test for DAT-54078

---------

Co-authored-by: fadiDL <99925584+fadiDL@users.noreply.github.com>

src/components/compound/DlSearches/DlSmartSearch/components/DlSmartSearchInput.vue
@@ -360,7 +360,16 @@ export default defineComponent({
 
         const debouncedSetInputValue = debounce(setInputValue, 300)
 
+        let lastSearchQuery: string;
+
         const updateJSONQuery = () => {
+
+            if (lastSearchQuery === searchQuery.value) {
+                return null
+            } else {
+                lastSearchQuery = searchQuery.value
+            }
+
             try {
                 const bracketless = removeBrackets(searchQuery.value)
                 const cleanedAliases = revertAliases(bracketless, aliases.value)

tests/DlSmartSearch/DlSmartSearchInput.spec.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import DlSmartSearchInput from '../../src/components/compound/DlSearches/DlSmartSearch/components/DlSmartSearchInput.vue'
-import { describe, it, expect, vi, beforeAll } from 'vitest'
+import { describe, it, expect, vi, beforeAll, beforeEach } from 'vitest'
 
 window.ResizeObserver =
     window.ResizeObserver ||
@@ -410,7 +410,9 @@ describe('DlSmartSearchInput', () => {
             // @ts-ignore // handled in jest setup
             await window.delay(500)
             await wrapper.vm.$nextTick()
+        })
 
+        beforeEach(async () => {
             wrapper.vm.onKeyPress({
                 key: 'Enter',
                 preventDefault: vi.fn(),
@@ -424,6 +426,10 @@ describe('DlSmartSearchInput', () => {
 
         it('will emit search with model value', () => {
             expect(wrapper.emitted().search).toEqual([[{ name: 'test' }]])
+        })
+
+        it('will not emit search when the query did not change', () => {
+            expect(wrapper.emitted().search.length).toBe(1)
         })
     })
 

commit 0cf3c8d76f3d6aaefc1a427dced6f29df48248f8

fixes #686
broken in #634

src/components/compound/DlSearches/DlSmartSearch/components/DlSmartSearchInput.vue
@@ -386,12 +386,6 @@ export default defineComponent({
         let lastSearchQuery: string
 
         const updateJSONQuery = () => {
-            if (lastSearchQuery === searchQuery.value) {
-                return null
-            } else {
-                lastSearchQuery = searchQuery.value
-            }
-
             try {
                 const bracketless = removeBrackets(searchQuery.value)
                 const cleanedAliases = revertAliases(bracketless, aliases.value)
@@ -610,10 +604,13 @@ export default defineComponent({
                 return
             }
 
-            const toSearch = updateJSONQuery()
-            if (toSearch) {
-                emit('search', toSearch)
-                showSuggestions.value = false
+            if (lastSearchQuery !== searchQuery.value) {
+                lastSearchQuery = searchQuery.value
+                const toSearch = updateJSONQuery()
+                if (toSearch) {
+                    emit('search', toSearch)
+                    showSuggestions.value = false
+                }
             }
         }
 