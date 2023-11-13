833984fee34ad6967ac536aacb0c4819bef829ed
Sorin <sorin.hadjiu@opentech.email>
2023-11-13 15:27:53 +0200

    fix pagination styles

--- src/components/compound/DlTable/DlTable.vue
+++ src/components/compound/DlTable/DlTable.vue
@@ -1110,7 +1110,7 @@ export default defineComponent({
          */
         selectedRowsLabel: {
             type: Function,
-            default: (val: number) => `${val} records selected`
+            default: (val: number) => null
         },
         /**
          * Label visible when loading is active
