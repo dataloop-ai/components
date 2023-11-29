010538fb66f27585fd135a60966debfc42962469
Fadi Atamny <fadi@dataloop.ai>
2023-11-29 16:50:49 +0200

    manual ellipsis calculationj

--- src/components/compound/DlPagination/components/PaginationLegend.vue
+++ src/components/compound/DlPagination/components/PaginationLegend.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="dl-pagination--legend">
         {{ paginationContent }}
+        <dl-tooltip v-if="longItemsLength">
+            {{ fullContent }}
+        </dl-tooltip>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue-demi'
+import { DlTooltip } from '../../../shared'
 
 export default defineComponent({
     name: 'PaginationLegend',
+    components: {
+        DlTooltip
+    },
     props: {
         from: {
             type: Number,
@@ -28,11 +35,43 @@ export default defineComponent({
         }
     },
     computed: {
+        longItemsLength(): boolean {
+            return String(this.total).length > 6
+        },
         paginationContent(): string {
-            return (
-                `Showing ${this.from}-${this.to}` +
-                (this.total ? ` of ${this.total} ${this.itemsName}` : '')
-            )
+            let totalItemsLength = String(this.total)
+            if (this.longItemsLength) {
+                totalItemsLength = `${totalItemsLength.slice(
+                    0,
+                    4
+                )}...${totalItemsLength.slice(-3)}`
+            }
+
+            let toDisplayString = String(this.to)
+            if (toDisplayString.length > 3) {
+                toDisplayString = `${toDisplayString.slice(
+                    0,
+                    2
+                )}...${toDisplayString.slice(-2)}`
+            }
+
+            let fromDisplayString = String(this.to)
+            if (fromDisplayString.length > 3) {
+                fromDisplayString = `${fromDisplayString.slice(
+                    0,
+                    2
+                )}...${fromDisplayString.slice(-2)}`
+            }
+
+            const header = `Showing ${fromDisplayString} - ${toDisplayString}`
+            const footer = this.total
+                ? `of ${totalItemsLength} ${this.itemsName}`
+                : ''
+
+            return `${header} ${footer}`
+        },
+        fullContent(): string {
+            return `Showing ${this.from} - ${this.to} of ${this.total} ${this.itemsName}`
         }
     }
 })
