f6ab7a9abb2e767936f0e3015d022decc441973e
advaa1234 <120374103+advaa1234@users.noreply.github.com>
2024-02-01 12:08:19 +0200

    tooltip (#853)
    * updated dl-stepper footer padding
    
    * added remove footer labels prop
    
    * npm i
    
    * 0.19.205
    
    * 0.19.206
    
    * dlsmartsearchinput product requested changes
    
    * 0.19.207
    
    * dltooltip - temporary changes in order for custom styling to work in vue2
    
    * 0.19.208

--- src/components/compound/DlSearches/DlSmartSearch/components/DlSmartSearchInput.vue
+++ src/components/compound/DlSearches/DlSmartSearch/components/DlSmartSearchInput.vue
@@ -17,7 +17,7 @@
                                 ? defaultIconColor
                                 : statusIconColor || defaultIconColor
                         "
-                        size="16px"
+                        size="12px"
                         :inline="false"
                     />
                 </div>
@@ -52,25 +52,24 @@
                 </div>
                 <dl-tooltip
                     v-if="!focused"
-                    :style="`
-                        background-color: var(--dl-color-panel-background);
-                        border: 1px solid var(--dl-color-separator);
-                        color: var(--dl-color-darker);
-                        padding: 16px;
-                    `"
+                    border="1px solid var(--dl-color-separator)"
+                    background-color="dl-color-panel-background"
+                    color="dl-color-darker"
                     max-width="340px"
                     anchor="bottom left"
                     self="top left"
                 >
-                    <div class="tooltip-title">
-                        <dl-icon :icon="defaultIcon" />
-                        Smart Search Query
-                    </div>
-                    <div class="tooltip-subtitle">
-                        A powerful and flexible search. Allows users to
-                        construct queries based on specific field selection
-                        following schema rules. Does not enable free text
-                        search.
+                    <div style="padding: 10px">
+                        <div class="tooltip-title">
+                            <dl-icon :icon="defaultIcon" />
+                            Smart Search Query
+                        </div>
+                        <div class="tooltip-subtitle">
+                            A powerful and flexible search. Allows users to
+                            construct queries based on specific field selection
+                            following schema rules. Does not enable free text
+                            search.
+                        </div>
                     </div>
                 </dl-tooltip>
             </div>
@@ -1161,8 +1160,8 @@ export default defineComponent({
     &__status-icon-wrapper {
         display: flex;
         line-height: 15px;
-        margin: 6px 8px 0px 0px;
-        align-items: flex-start;
+        margin: 3px 8px 0px 0px;
+        align-items: center;
         div:first-child {
             display: flex;
             align-items: center;
