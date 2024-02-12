d242ab530c227938f17bd8dd32c5c43b2177a96d
Fadi Atamny <fadi@dataloop.ai>
2024-02-12 15:34:57 +0200

    handled all vulnerability fixes

--- src/components/compound/DlInput/DlInput.vue
+++ src/components/compound/DlInput/DlInput.vue
@@ -802,7 +802,9 @@ export default defineComponent({
                         return `<span class="clickable" style="color: ${syntaxHighlightColor.value}">${word}</span>`
                     }
                     if (word.startsWith('<')) {
-                        return `<span>${word.replace('<', '&lt;')}</span>`
+                        return `<span>${word
+                            .replace('<', '&lt;')
+                            .replace('>', '&gt;')}</span>`
                     }
                     return word
                 })
