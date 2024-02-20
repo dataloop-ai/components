0fbb2bdf1d9fd899ff36588f7cc29e6c1852010c
makc-dataloop <maksym.z@dataloop.ai>
2024-02-20 10:36:44 +0400

    add DlDateTimePicker (fixes #841)

---
+++ src/demos/DlDateTimePickerDemo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div>
+            <dl-date-time-picker
+                v-model="date"
+                show-time
+                @change="handleModelValueUpdate"
+            />
+        </div>
+        <div>
+            {{ date }}
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue-demi'
+import { DlDateTimePicker } from '../components'
+
+export default defineComponent({
+    components: {
+        DlDateTimePicker
+    },
+    data(): {
+        date: Date
+    } {
+        return {
+            date: null
+        }
+    },
+    computed: {},
+    methods: {
+        handleModelValueUpdate(value: Date) {
+            console.log('Date: ', value)
+        }
+    }
+})
+</script>
+<style lang="scss" scoped>
+.width-300 {
+    width: 300px;
+}
+</style>
