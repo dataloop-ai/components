be41c29134262b5a918589e94aa4d74af2e4bf0a
makc-dataloop <146977958+makc-dataloop@users.noreply.github.com>
2024-02-27 16:06:36 +0400

    DAT-64142: DlBarChart improvements (#887)
    Co-authored-by: guyDataloop <71375976+guyDataloop@users.noreply.github.com>

--- src/demos/BarChartDemo.vue
+++ src/demos/BarChartDemo.vue
@@ -4,7 +4,7 @@
             display: flex;
             width: 900px;
             flex-wrap: wrap;
-            flex-direction: row;
+            flex-direction: column;
             gap: 0px;
         "
     >
@@ -22,6 +22,19 @@
             :items-in-view="6"
             is-empty
         />
+
+        <div style="color: var(--dl-color-darker); margin-bottom: 30px">
+            Customized DlBarChart with data labels
+        </div>
+        <!-- @vue-ignore -->
+        <dl-bar-chart
+            :data="cdlData"
+            :options="cdlOptions"
+            :plugins="cdlPlugins"
+            :items-in-view="data.labels.length + 1"
+        >
+            <template #legend><span /></template>
+        </dl-bar-chart>
     </div>
 </template>
 
@@ -29,6 +42,7 @@
 import { defineComponent } from 'vue-demi'
 import { ChartOptions } from 'chart.js'
 import { DlBarChart } from '../components'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
 
 const labelsFn = () => {
     const a = []
@@ -78,6 +92,76 @@ const options: ChartOptions = {
     }
 }
 
+const cdlData = {
+    labels: labelsFn().map((l) => `Data [${l}]:`),
+    datasets: [
+        {
+            label: 'Duh',
+            backgroundColor: '--dl-color-secondary',
+            borderRadius: 4,
+            data: dataFn(),
+            datalabels: {
+                align: 'end',
+                anchor: 'start'
+            }
+        },
+        {
+            label: 'Duh',
+            backgroundColor: '--dl-color-info-background',
+            borderRadius: 4,
+            data: dataFn().map((n) => 20 - n),
+            datalabels: {
+                align: 'start',
+                anchor: 'end'
+            }
+        }
+    ]
+}
+
+const cdlOptions: ChartOptions = {
+    plugins: {
+        datalabels: {
+            color: '--dl-color-darker',
+            font: {
+                weight: 'bold'
+            },
+            formatter (value, context) {
+                switch (context.datasetIndex) {
+                    case 0:
+                        return context.chart.data.labels[context.dataIndex]
+                    case 1:
+                        return (
+                            'Value is ' +
+                            context.chart.data.datasets[0].data[
+                                context.dataIndex
+                            ]
+                        )
+                }
+            }
+        },
+        tooltip: {
+            callbacks: {
+                label (tooltipItem) {
+                    return 'Value is ' + tooltipItem.formattedValue
+                }
+            },
+            filter (tooltipItem) {
+                return tooltipItem.datasetIndex === 0
+            }
+        }
+    },
+    scales: {
+        x: {
+            stacked: true,
+            display: false
+        },
+        y: {
+            stacked: true,
+            display: false
+        }
+    }
+}
+
 export default defineComponent({
     name: 'DlChartDemo',
     components: {
@@ -85,6 +169,9 @@ export default defineComponent({
     },
     data() {
         return {
+            cdlData,
+            cdlOptions,
+            cdlPlugins: [ChartDataLabels],
             data,
             options,
             legendProps
