7773b7669a6a4c2bd9b2e2092e86c6a30b3acb48
advaa1234 <120374103+advaa1234@users.noreply.github.com>
2024-05-22 13:20:45 +0300

    type Date instead of Object (#930)
    Co-authored-by: fadiDL <99925584+fadiDL@users.noreply.github.com>

--- src/components/compound/DlDateTime/DlDateTimeRange/DlDateTimePicker.vue
+++ src/components/compound/DlDateTime/DlDateTimeRange/DlDateTimePicker.vue
@@ -51,7 +51,7 @@ export default defineComponent({
     },
     props: {
         modelValue: {
-            type: Object as PropType<Date | null>,
+            type: Date,
             default: null
         },
         showTime: Boolean,
