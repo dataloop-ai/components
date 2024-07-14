d6c38772d1fc2f73619ce2a26b7ad274cc392eeb
venkateshnair183 <129487905+venkateshnair183@users.noreply.github.com>
2024-07-14 23:36:08 +0400

    PR-comments-fix

--- src/components/compound/DlInput/DlTextInput.vue
+++ src/components/compound/DlInput/DlTextInput.vue
@@ -591,13 +591,8 @@ export default defineComponent({
         onChange(e: any): void {
             this.isMenuOpen = true
             const trimmedValue = e.target.value.trim()
-            if (trimmedValue !== e.target.value) {
-                this.$emit('input', trimmedValue, e)
-                this.$emit('update:model-value', trimmedValue)
-            } else {
-                this.$emit('input', e.target.value, e)
-                this.$emit('update:model-value', e.target.value)
-            }
+            this.$emit('input', trimmedValue, e)
+            this.$emit('update:model-value', trimmedValue)
         },
         focus(): void {
             const inputRef = this.$refs.input as HTMLInputElement
