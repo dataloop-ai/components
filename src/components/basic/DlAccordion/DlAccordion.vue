9bc2f63d7ee33d7ef84e524a963fe944d151e842
ai-hemant <125646532+ai-hemant@users.noreply.github.com>
2025-07-23 12:35:38 +0400

    feat: adds background mode to accordion DAT-84067 (#1040)
    * feat: adds background mode to accordion DAT-84067
    
    * Update DlAccordionDemo.vue

--- src/components/basic/DlAccordion/DlAccordion.vue
+++ src/components/basic/DlAccordion/DlAccordion.vue
@@ -3,16 +3,22 @@
         :id="uuid"
         :class="identifierClass"
         class="accordion"
+        :style="accordionContentStyles"
     >
         <dl-accordion-header
             :additional-info="additionalInfo"
             :default-opened="defaultOpened"
             :title="title"
             :font-size="fontSize"
+            :font-weight="fontWeight"
             :title-color="titleColor"
             :right-side="rightSide"
             :is-open="isOpen"
+            :closed-icon="closedIcon"
+            :opened-icon="openedIcon"
             data-test-id="accordion-header"
+            :background-color="backgroundColor"
+            :with-background="withBackground"
             @click="handleClick"
         >
             <template #header>
@@ -29,18 +35,9 @@
             }"
         >
             <slot v-if="isOpen && !isEmpty" />
-            <dl-empty-state
-                v-if="isOpen && isEmpty"
-                v-bind="emptyStateProps"
-            >
-                <template
-                    v-for="(_, slot) in $slots"
-                    #[slot]="props"
-                >
-                    <slot
-                        :name="slot"
-                        v-bind="props"
-                    />
+            <dl-empty-state v-if="isOpen && isEmpty" v-bind="emptyStateProps">
+                <template v-for="(_, slot) in $slots" #[slot]="props">
+                    <slot :name="slot" v-bind="props" />
                 </template>
             </dl-empty-state>
         </div>
@@ -53,6 +50,7 @@ import DlEmptyState from '../DlEmptyState/DlEmptyState.vue'
 import { DlEmptyStateProps } from '../DlEmptyState/types'
 import { defineComponent, PropType } from 'vue-demi'
 import { v4 } from 'uuid'
+import { getColor } from '../../../../utils'
 
 const accordionEmptyStateProps = {
     title: '',
@@ -77,6 +75,7 @@ export default defineComponent({
         defaultOpened: { type: Boolean, default: false },
         title: { type: String, default: null },
         fontSize: { type: String, default: '12px' },
+        fontWeight: { type: String, default: '400' },
         titleColor: { type: String, default: 'dl-color-medium' },
         modelValue: { type: Boolean, default: null },
         rightSide: { type: Boolean, default: false },
@@ -85,7 +84,12 @@ export default defineComponent({
             type: Object as PropType<DlEmptyStateProps>,
             default: () => accordionEmptyStateProps
         },
-        separator: { type: Boolean, default: false }
+        separator: { type: Boolean, default: false },
+        closedIcon: { type: String, default: 'icon-dl-right-chevron' },
+        openedIcon: { type: String, default: 'icon-dl-down-chevron' },
+        backgroundColor: { type: String, default: 'dl-color-fill' },
+        withBackground: { type: Boolean, default: false },
+        iconHoverColor: { type: String, default: 'dl-color-primary' }
     },
     emits: ['update:model-value', 'hide', 'show'],
     data() {
@@ -101,6 +105,23 @@ export default defineComponent({
         },
         hasHeaderSlot(): boolean {
             return this.$slots.header !== undefined
+        },
+        accordionContentStyles(): Record<string, string> {
+            return {
+                '--dl-color-accordion-content-background':
+                    this.withBackground && this.isOpen
+                        ? getColor(this.backgroundColor)
+                        : '',
+                '--dl-accordion-content-padding': this.withBackground
+                    ? '16px'
+                    : '0 16px 15px 38px',
+                '--dl-accordion-content-border-radius': this.withBackground
+                    ? '4px'
+                    : '',
+                '--dl-accordion-margin-left': this.withBackground
+                    ? '12px'
+                    : '0px'
+            }
         }
     },
     methods: {
@@ -118,13 +139,14 @@ export default defineComponent({
 <style scoped lang="scss">
 .accordion {
     max-width: 100%;
+    margin-left: var(--dl-accordion-margin-left);
 }
 .accordion-content {
     text-align: left;
     font-size: var(--dl-font-size-body);
     transition: all 300ms;
     line-height: 16px;
-    padding: 0 16px 15px 38px;
+    padding: var(--dl-accordion-content-padding, 0 16px 15px 38px);
     color: var(--dl-color-darker);
     max-height: fit-content;
     overflow: hidden;
@@ -138,5 +160,7 @@ export default defineComponent({
         border-color: transparent;
         padding-bottom: 0;
     }
+    background-color: var(--dl-color-accordion-content-background);
+    border-radius: var(--dl-accordion-content-border-radius);
 }
 </style>
