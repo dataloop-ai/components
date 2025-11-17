d00ad0399ff8d4367d0a440ad532b2fef4f1ccc6
Yonatan Mesika <yonatan.m@dataloop.ai>
2025-11-17 17:08:33 +0200

    Fix build issue

--- src/components/compound/DlSearches/DlSearch/DlSearch.vue
+++ src/components/compound/DlSearches/DlSearch/DlSearch.vue
@@ -160,12 +160,16 @@ export default defineComponent({
             this.$emit('search', this.modelValue)
         },
         focus(): void {
-            const inputComponent = this.$refs.input as typeof DlInput
-            inputComponent.focus()
+            const inputComponent = this.$refs.input as InstanceType<
+                typeof DlInput
+            >
+            inputComponent?.focus()
         },
         blur(): void {
-            const inputComponent = this.$refs.input as typeof DlInput
-            inputComponent.blur()
+            const inputComponent = this.$refs.input as InstanceType<
+                typeof DlInput
+            >
+            inputComponent?.blur()
         }
     }
 })
