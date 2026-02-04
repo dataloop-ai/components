4aceae44f39a264a8572fa027cec3beae26f56b6
venkateshnair183 <129487905+venkateshnair183@users.noreply.github.com>
2026-02-05 02:49:40 +0400

    DAT-121411-alert-button-changes

--- src/components/compound/DlToast/components/ToastComponent.vue
+++ src/components/compound/DlToast/components/ToastComponent.vue
@@ -61,21 +61,9 @@
                         :key="idx"
                         :label="action.label"
                         :icon="action.icon"
-                        :color="action.textColor || 'dl-alert-border-color'"
-                        :text-color="
-                            action.textColor || 'dl-alert-border-color'
-                        "
-                        :hover-border-color="
-                            action.textColor || 'dl-alert-border-color'
-                        "
-                        :hover-text-color="
-                            action.textColor || 'dl-alert-border-color'
-                        "
-                        :hover-bg-color="'transparent'"
-                        :pressed-bg-color="'transparent'"
-                        :styles="action.styles || defaultActionButtonStyles"
-                        :filled="false"
+                        color="secondary"
                         outlined
+                        :styles="action.styles || defaultActionButtonStyles"
                         no-wrap
                         overflow
                         @click="onActionClick(action)"
