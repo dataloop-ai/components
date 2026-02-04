27994f3e709fa966ad24cdc11ddb600b8b5d12ed
yogesh0325 <125871155+yogesh0325@users.noreply.github.com>
2026-02-04 16:34:23 +0400

    Update css

--- src/components/compound/DlTreeTable/views/DlTrTreeView.vue
+++ src/components/compound/DlTreeTable/views/DlTrTreeView.vue
@@ -11,10 +11,10 @@
         @mouseleave="onRowHoverEnd($event, row, rowIndex)"
     >
         <td
-            :style="`width: 25px; opacity: ${
+            :style="`opacity: ${
                 isDragIconVisible || isRowHighlighted ? '1' : '0'
             }`"
-            class="draggable-icon-container"
+            class="dl-draggable-container"
         >
             <dl-icon
                 v-if="hasDraggableRows && !row.disableDraggable"
@@ -452,5 +452,8 @@ export default defineComponent({
     &.no-children {
         cursor: default;
     }
+}
+.dl-draggable-container {
+    width: 25px;
 }
 </style>
