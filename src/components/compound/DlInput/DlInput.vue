edd16e05023b8d55e88945e1a073e1b6d542e034
Yoav Yamin <yoav.y@dataloop.ai>
2026-04-26 15:19:20 +0300

    revert

--- src/components/compound/DlInput/DlInput.vue
+++ src/components/compound/DlInput/DlInput.vue
@@ -1563,7 +1563,7 @@ export default defineComponent({
         white-space: var(--dl-input-white-space);
         font-size: var(--dl-typography-body-body2-font-size);
         font-weight: var(--dl-typography-body-body2-font-weight);
-        overflow: hidden scroll;
+        overflow: hidden;
         text-overflow: ellipsis;
         box-sizing: content-box;
         word-wrap: break-word;
@@ -1584,9 +1584,7 @@ export default defineComponent({
         }
 
         &--prepend {
-            flex: 1;
-            min-width: 0;
-            width: auto;
+            width: calc(100% - 10px - 28px);
         }
 
         &--append {
